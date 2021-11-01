d18375a7511b3fe85ebbabe0018572cdb08b7fef
jkl851 <jkl851@naver.com>
2021-11-01 15:11:11 +0900

    # Springboot-practices

---
+++ helloworld/src/main/java/ex01/HelloWorldApplication.java
@@ -0,0 +1,43 @@
+package ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/*
+ * 
+ *  Bootstrapping Class
+ *  
+ *  1. 스프링 부트 어플리케이션에 부트스트래핑(Bootstrapping)
+ *  2. 설정 클래스로 역할 (Configuration Class)
+ * 
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. 어플리케이션 컨텍스트(Application Context, Spring Container) 생성
+		 * 2. 웹 어플리케이션 타입(Web Application인 경우, MVC or Reactive)
+		 * 3. 어노테이션 스캐닝(Auto) or Configuration 클래스(Explicit) 통한 빈 생성 및 등록
+		 * 4. 웹 어플리케이션인 경우(MVC)
+		 * 	  - 내장(embedded) 서버 (TomcatEmbeddedServletContainer) 인스턴스 생성
+		 * 	  - 서버 인스턴스에 웹 어플리케이션을 배포
+		 * 	  - 서버 인스턴스 실행
+		 * 5. 인터페이스 ApplicationRunner 구현 빈의 run() 실행
+		 */
+//		ConfigurableApplicationContext c = null;
+//		try {
+//			c = SpringApplication.run(HelloWorldApplication.class, args);
+//		} catch(Throwable ex) {
+//			ex.printStackTrace()
+//		} finally {
+//			c.close();
+//		}
+		
+//      try~with~resource
+		try(ConfigurableApplicationContext c = SpringApplication.run(HelloWorldApplication.class, args)) {
+		} 
+	}
+
+}
